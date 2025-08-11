ec5caa18e43b3efd1cacb9ac0765f27a6c7a872e
Rizel Thomas <rizel.a@icloud.com>
2025-08-11 10:21:49 +0200

    updated jest configuration for dropdown tests. also updated integration tests

--- widgets/dropdown/tests/TrimmDropdown.integration.test.tsx
+++ widgets/dropdown/tests/TrimmDropdown.integration.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen, fireEvent, waitFor, act } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { act } from "react";
 import { TrimmDropdown } from "../src/TrimmDropdown";
 import { createElement } from "react";
 
